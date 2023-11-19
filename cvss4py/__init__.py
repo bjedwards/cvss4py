bf041a70739baf7e9e69b37bbb7cbd8dc4c0937e
Ben Edwards <ben@cyentia.com>
2023-11-19 11:05:46 -0600

    Cleanup package information

--- cvss4py/__init__.py
+++ cvss4py/__init__.py
@@ -1,8 +1,12 @@
-import json
-import pkgutil
+__all__ = ["data", "vector_str_to_object"]
 
-metric_fname = pkgutil.get_data("data", "metric_data.json")
+def __dir__():
+    return __all__
 
-with open(metric_fname, "r") as f:
-    metric_data = json.load(f)
+def __getattr__(name):
+    if name not in __all__:
+        raise AttributeError(name)
+    return globals()[name]
 
+from .data import *
+from .validate import *
