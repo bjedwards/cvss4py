3d0db2d13d57ab5ac1318e5d2fc42e9b094bdca7
Ben Edwards <ben@cyentia.com>
2023-11-30 11:05:59 -0600

    Data restructuring

--- cvss4py/data/__init__.py
+++ cvss4py/data/__init__.py
@@ -1,4 +1,5 @@
-__all__ = ["metric_values", "metric_categories", "metric_defaults"]
+__all__ = ["metric_values", "metric_categories", "metric_defaults", 
+           "eq_class_values", "eq_class_scores", "eq_class_max_subvectors", "eq_class_metrics"]
 
 def __dir__():
     return __all__
@@ -11,10 +12,21 @@ def __getattr__(name):
 import json
 import pkgutil
 
-raw_data = pkgutil.get_data("cvss4py", "data/metric_data.json")
+raw_metric_data = pkgutil.get_data("cvss4py", "data/metric_data.json")
 
-metric_data = json.loads(raw_data)
+metric_data = json.loads(raw_metric_data)
 
 metric_values = metric_data['metric_values']
 metric_categories = metric_data['metric_categories']
 metric_defaults = metric_data['metric_defaults']
+metric_distances = metric_data['metric_distances']
+
+raw_eq_data =  pkgutil.get_data("cvss4py", "data/eq_data.json")
+eq_data = json.loads(raw_eq_data)
+
+eq_class_metrics = eq_data['eq_class_metrics']
+eq_class_max_subvectors = eq_data['eq_class_max_subvectors']
+eq_class_values = eq_data['eq_class_values']
+eq_class_scores = eq_data['eq_class_scores']
+eq_class_max_distance = eq_data["eq_class_max_distances"]
+
